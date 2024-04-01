d4231461c77f48de2e7dc66ffda408505b44e9c7
Ilyann <ilyan.gregorio@ynov.com>
2024-04-01 18:50:14 +0200

    groupie

---
+++ view/viewMain.go
@@ -0,0 +1,20 @@
+package view
+
+import (
+	"groupietracker/model"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+)
+
+var (
+	myApp model.AppData
+)
+
+func Start() {
+	myApp.App = app.New()
+	myApp.Window = myApp.App.NewWindow("Groupie Tracker.")
+	PageMain()
+	myApp.Window.Resize(fyne.NewSize(225, 875))
+	myApp.Window.ShowAndRun()
+}
