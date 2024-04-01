77f31a1051acac0a8cfdb06188849ead33066978
Ilyann <ilyan.gregorio@ynov.com>
2024-04-01 18:56:29 +0200

    groupie

--- model/location.go
+++
@@ -1,6 +0,0 @@
-package model
-
-type Locations struct {
-	Id       int64    `json:"id"`
-	Location []string `json:"locations"`
-}
--- model/relation.go
+++
@@ -1,6 +0,0 @@
-package model
-
-type Relation struct {
-	Id             int64               `json "id"`
-	DatesLocations map[string][]string `json:"datesLocations"`
-}
